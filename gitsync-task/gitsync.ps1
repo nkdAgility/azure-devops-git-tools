0022719b5fd8723c9839aef845ec391905568847
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2016-10-14 12:57:44 -0500

    And updated

--- gitsync-task/gitsync.ps1
+++ gitsync-task/gitsync.ps1
@@ -8,10 +8,16 @@ Write-VstsTaskVerbose "GitRepoUrl: $GitRepoUrl"
 
 Set-Location $Env:BUILD_SOURCESDIRECTORY
 
+### Add teh URL
 Write-VstsTaskVerbose ">>git remote add target $GitRepoUrl"
 git remote add target $GitRepoUrl
+## Checkout the Branch
+Write-VstsTaskVerbose ">>git checkout $Env:Build_SourceBranchName"
+git checkout $Env:Build_SourceBranchName
+## Pull stuff
 Write-VstsTaskVerbose ">>git pull target $Env:Build_SourceBranchName --tags"
 git pull target $Env:Build_SourceBranchName --tags
+## Push stuff
 Write-VstsTaskVerbose ">>git push target --all"
 git push target --all
 Write-VstsTaskVerbose "??git push target HEAD:$Env:Build_SourceBranchName --tags"
